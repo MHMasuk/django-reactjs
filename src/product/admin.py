d62cbe1669e92500643a1f70a4027532d6778c5a
MHMasuk <masuk@w3engineers.com>
2022-03-30 15:42:15 +0600

    Add pagination and list view

--- src/product/admin.py
+++ src/product/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from .models import Product, Variant, ProductVariantPrice, ProductVariant
 # Register your models here.
+
+admin.site.register(Product)
+admin.site.register(Variant)
+admin.site.register(ProductVariantPrice)
+admin.site.register(ProductVariant)
+
--- src/product/views/product.py
+++ src/product/views/product.py
@@ -1,6 +1,6 @@
 from django.views import generic
 
-from product.models import Variant
+from product.models import Variant, Product, ProductVariantPrice, ProductVariant
 
 
 class CreateProductView(generic.TemplateView):
@@ -12,3 +12,26 @@ class CreateProductView(generic.TemplateView):
         context['product'] = True
         context['variants'] = list(variants.all())
         return context
+
+
+class ProductListView(generic.ListView):
+    template_name = 'products/list.html'
+    paginate_by = 10
+
+    def get_queryset(self):
+        filter_string = {}
+        print("ProductListView", self.request.GET)
+        for key in self.request.GET:
+            if self.request.GET.get(key):
+                filter_string[key] = self.request.GET.get(key)
+        return Product.objects.filter(**filter_string)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['product'] = True
+        context['variants'] = Variant.objects.all()
+        context['request'] = ''
+        if self.request.GET:
+            context['request'] = self.request.GET['title__icontains']
+        return context
+
